c9203a3605a8ccfe42e768ac4095c82acf44c1be
takenoko-gohan <bamboo.shoot.rice@gmail.com>
2021-04-14 15:08:41 +0000

    不要な箇所を削除

--- app/index.js
+++ app/index.js
@@ -7,111 +7,72 @@ const wasm = require("./wasm/image_resize");
 
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3({
-  signatureVersion: 'v4',
+    signatureVersion: 'v4',
 });
-//const Sharp = require('sharp');
 
-// set the S3 endpoints
-const BUCKET = 'takenoko-test-images';
+// 自環境の S3 バケットを指定してください
+const BUCKET = '';
 
 exports.handler = (event, context, callback) => {
-  let response = event.Records[0].cf.response;
-
-  console.log("Response status code :%s", response.status);
-
-  //check if image is not present
-  if (response.status == 404) {
-
+    let response = event.Records[0].cf.response;
+ 
     let request = event.Records[0].cf.request;
     let params = querystring.parse(request.querystring);
 
-    // if there is no dimension attribute, just pass the response
+    console.log(JSON.stringify(params));
+
+    // パラメーター d の指定がない場合、そのまま返す
     if (!params.d) {
-      callback(null, response);
-      return;
+        callback(null, response);
+        return;
     }
 
-    // read the dimension parameter value = width x height and split it by 'x'
-    let dimensionMatch = params.d.split("x");
-
-    // read the required path. Ex: uri /images/100x100/webp/image.jpg
+    // リサイズ後の幅と高さを取得する
+    let dimension = params.d.split("x");
+    let width = dimension[0];
+    let height = dimension[1];
+  
+    // URI から必要な情報を取得
     let path = request.uri;
 
-    // read the S3 key from the path variable.
-    // Ex: path variable /images/100x100/webp/image.jpg
     let key = path.substring(1);
 
-    // parse the prefix, width, height and image name
-    // Ex: key=images/200x200/webp/image.jpg
-    let prefix, originalKey, match, width, height, requiredFormat, imageName;
-    let startIndex;
+    let prefix, originalKey, match, imageName, requiredFormat;
 
     try {
-      match = key.match(/(.*)\/(\d+)x(\d+)\/(.*)\/(.*)/);
-      prefix = match[1];
-      width = parseInt(match[2], 10);
-      height = parseInt(match[3], 10);
+        match = key.match(/(.*)\/(.*)/);
+        prefix = match[1];
 
-      // correction for jpg required for 'Sharp'
-      requiredFormat = match[4] == "jpg" ? "jpeg" : match[4];
-      imageName = match[5];
-      originalKey = prefix + "/" + imageName;
+        imageName = match[2];
+        requiredFormat = imageName.split(".")[1];
+        
+        originalKey = prefix + "/" + imageName;
     }
     catch (err) {
-      // no prefix exist for image..
-      console.log("no prefix present..");
-      match = key.match(/(\d+)x(\d+)\/(.*)\/(.*)/);
-      width = parseInt(match[1], 10);
-      height = parseInt(match[2], 10);
+        console.log("no prefix present..");
+        match = key.match(/(.*)/);
 
-      // correction for jpg required for 'Sharp'
-      requiredFormat = match[3] == "jpg" ? "jpeg" : match[3]; 
-      imageName = match[4];
-      originalKey = imageName;
+        imageName = match[1];
+
+        requiredFormat = imageName.split(".")[1];
+        
+        originalKey = imageName;
     }
 
-    // get the source image file
+    // 画像を取得し、リサイズ
     console.log("start getObject");
     S3.getObject({ Bucket: BUCKET, Key: originalKey }).promise()
-      //.then(data => wasm.resize(data.Body.toString("base64"), width, height))
-      .then(data => wasm.resize(data.Body, width, height))
-      // perform the resize operation
-      //.then(data => Sharp(data.Body)
-      //  .resize(width, height)
-      //  .toFormat(requiredFormat)
-      //  .toBuffer()
-      //)
-      .then(result => {
-        //let buffer = Buffer.from(result);
-        //let buffer = wasm.resize(data.Body, width, height);
-        
-        // save the resized object to S3 bucket with appropriate object key.
-        //S3.putObject({
-        //    Body: buffer,
-        //    Bucket: BUCKET,
-        //    ContentType: 'image/' + requiredFormat,
-        //    CacheControl: 'max-age=31536000',
-        //    Key: key,
-        //    StorageClass: 'STANDARD'
-        //}).promise()
-        //// even if there is exception in saving the object we send back the generated
-        //// image back to viewer below
-        //.catch(() => { console.log("Exception while writing resized image to bucket")});
-
-        // generate a binary response with resized image
-        response.status = 200;
-        response.body = result//buffer.toString('base64');
-        response.bodyEncoding = 'base64';
-        response.headers['content-type'] = [{ key: 'Content-Type', value: 'image/' + requiredFormat }];
-        console.log("response :", JSON.stringify(response));
-        callback(null, response);
-      })
+        .then(data => wasm.resize(data.Body, width, height, requiredFormat))
+        .then(result => {
+            // generate a binary response with resized image
+            response.status = 200;
+            response.body = result;
+            response.bodyEncoding = 'base64';
+            response.headers['content-type'] = [{ key: 'Content-Type', value: 'image/' + requiredFormat }];
+            console.log("response :", JSON.stringify(response));
+            callback(null, response);
+        })
     .catch( err => {
-      console.log("Exception while reading source image :%j",err);
+        console.log("Exception while reading source image :%j",err);
     });
-  } // end of if block checking response statusCode
-  else {
-    // allow the response to pass through
-    callback(null, response);
-  }
 };
--- image_resize/src/lib.rs
+++ image_resize/src/lib.rs
@@ -3,7 +3,7 @@ extern crate console_error_panic_hook;
 extern crate wasm_bindgen;
 extern crate web_sys;
 
-use image::{self, imageops::FilterType, GenericImageView};
+use image::{self, imageops::FilterType};
 use wasm_bindgen::prelude::*;
 
 macro_rules! log {
@@ -13,45 +13,48 @@ macro_rules! log {
 }
 
 #[wasm_bindgen]
-//pub fn resize(b: String, width: u32, height: u32) -> String {
-pub fn resize(buf: Vec<u8>, width: u32, height: u32) -> String {
+pub fn resize(buf: Vec<u8>, width: u32, height: u32, format: &str) -> String {
     console_error_panic_hook::set_once();
 
     log!("start wasm function");
 
-    //log!("start base64 decode");
-    //let buf = base64::decode(b).expect("base64 decode error");
-    //log!("end base64 decode");
-
+    // nodejs から渡されたバッファーを読み込む
     log!("start buffer load");
-    //let img = image::load_from_memory_with_format(&buf, image::ImageFormat::Jpeg)
-    let img = image::load_from_memory(&buf)
-        .expect("buffer load error");
+    let img = image::load_from_memory(&buf).expect("buffer load error");
     log!("end buffer load");
 
-    let old_w = img.width() as u32;
-    let old_h = img.height() as u32;
-    log!("old size: {} x {}", old_w, old_h);
-
+    // リサイズ
     log!("start image resize");
     let resize_image = img.resize_to_fill(width, height, FilterType::Triangle);
-    //let resize_image = img.resize_to_fill(width, height, FilterType::CatmullRom);
-    //let resize_image = img.resize_to_fill(width, height, FilterType::Lanczos3);
     log!("end image resize");
 
-    let new_w = resize_image.width() as u32;
-    let new_h = resize_image.height() as u32;
-    log!("new size: {} x {}", new_w, new_h);
-
+    // リサイズ結果を書き込む
     log!("start image write to buffer");
     let mut result: Vec<u8> = Vec::new();
-    match resize_image.write_to(&mut result, image::ImageOutputFormat::Jpeg(90)) {
-        Ok(_) => log!("buffer write sucess"),
-        Err(err) => log!("buffer write error: {}", err),
+
+    match format {
+        "jpeg" | "jpg" => {
+            log!("match jpeg");
+            match resize_image.write_to(&mut result, image::ImageOutputFormat::Jpeg(90)) {
+                Ok(_) => log!("buffer write sucess"),
+                Err(err) => log!("buffer write error: {}", err),
+            }
+        },
+        "png" => {
+            log!("match png");
+            match resize_image.write_to(&mut result, image::ImageOutputFormat::Png) {
+                Ok(_) => log!("buffer write sucess"),
+                Err(err) => log!("buffer write error: {}", err),
+            }
+        },
+        _ => {
+            log!("did not match");
+        },
     }
     log!("end image write to buffer");
 
     log!("end wasm function");
 
+    // BASE64 で返す
     return base64::encode(&result);
 }
